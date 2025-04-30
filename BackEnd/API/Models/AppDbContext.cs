bdc26d59820f006fd3ef46a84245174d72c4442c
proGamalSherif <jemyxsherifx@gmail.com>
2025-04-30 09:57:44 +0300

    Initial commit

---
+++ BackEnd/API/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+        }
+    }
+}
