35025d2067f0e56f8555a6abadade10fba1adc8f
proGamalSherif <jemyxsherifx@gmail.com>
2025-04-30 13:00:27 +0300

    finish manage employee

---
+++ FrontEnd/src/services/alert.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+  constructor() { }
+  success(message: string, title: string = 'Success') {
+    Swal.fire({
+      icon: 'success',
+      title,
+      text: message,
+      confirmButtonColor: '#198754'
+    });
+  }
+  error(message: string, title: string = 'Error') {
+    Swal.fire({
+      icon: 'error',
+      title,
+      text: message,
+      confirmButtonColor: '#dc3545'
+    });
+  }
+  confirm(message: string, title: string = 'Are you sure?') {
+    return Swal.fire({
+      icon: 'question',
+      title,
+      text: message,
+      showCancelButton: true,
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'Cancel',
+      confirmButtonColor: '#0d6efd',
+      cancelButtonColor: '#6c757d'
+    });
+  }
+  showLoading(title: string = 'Loading...', message: string = 'Please wait') {
+    Swal.fire({
+      title,
+      text: message,
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      didOpen: () => {
+        Swal.showLoading();
+      }
+    });
+  }
+  close() {
+    Swal.close();
+  }
+}
